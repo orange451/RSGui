95d63f5266497b5a52cd5497a7bea9157682c3f9
Andrew Hamilton <drew.hamilton@mac.com>
2019-05-30 11:16:04 -0400

    Removed duplicated files.

--- src/scripts/backend/RSGuiMouseListener.java
+++
@@ -1,7 +0,0 @@
-package scripts.gui.backend;
-
-public interface RSGuiMouseListener {
-	public boolean onMousePress( int x, int y );
-	public void onMouseDown( int x, int y );
-	public void onMouseUpdate( int x, int y );
-}
